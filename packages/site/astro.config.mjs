c34850a8b451a816c58a94373a98d71815a83048
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-06-19 10:34:44 -0400

    added vercel adapter

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig, envField } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
   output: `server`,
+  adapter: vercel(),
   experimental: {},
 });
+
